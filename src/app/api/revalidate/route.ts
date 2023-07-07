eda24d47aee71280f569f8486e0154e0842c3642
Kunal Shetye <kunal.shetye@gmail.com>
2023-07-07 23:31:41 +0200

    added logic for verifying the re-validation request

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -2,10 +2,15 @@ import {revalidatePath, revalidateTag} from 'next/cache';
 
 export async function POST(request: Request) {
     const { searchParams } = new URL(request.url);
+    const cfWorker = request.headers.get("cf-worker");
     console.log(await request.json());
-    revalidateTag("cities");
-    revalidateTag("city");
-    revalidatePath("/");
+    console.log();
+    if(cfWorker !== null && cfWorker !== '' && cfWorker === 'cg.optimizely.com'){
+        // verify that the cache invalidation is coming from cg.optimizely.com
+        revalidateTag("cities");
+        revalidateTag("city");
+        revalidatePath("/");
+    }
     return new Response(
         null,
         {
