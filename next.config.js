b32fdb386fb0c18b157c15451e57431fad00c332
Kunal Shetye <kunal.shetye@gmail.com>
2023-07-07 21:09:46 +0200

    added more domains

--- next.config.js
+++ next.config.js
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'bank.opti-demo.xyz',
-          pathname: '/globalassets/**',
-        },
-        {
-          protocol: 'https',
-          hostname: 'images2.welcomesoftware.com',
-        },
-        {
-            protocol: 'https',
-            hostname: 'source.unsplash.com',
-        }
-    ],
-  },
+    reactStrictMode: true,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '*.opti-demo.xyz',
+                pathname: '/globalassets/**',
+            },
+            {
+                protocol: 'https',
+                hostname: '*.welcomesoftware.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'source.unsplash.com',
+            }
+        ],
+    },
 }
 
 module.exports = nextConfig
